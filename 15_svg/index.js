e86222d0a8d79066285e8e0c6d8b4906b8a5cf1b
tinawong15 <tinaw425@gmail.com>
2019-03-21 23:51:51 -0400

    in progress

---
+++ 15_svg/index.js
@@ -0,0 +1,31 @@
+var data = [39, 40, 44, 47, 41, 37];
+
+var width = 420,
+    barHeight = 20;
+
+var x = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, width]);
+    
+var chart = d3.select(".chart")
+    .attr("width", width)
+    .attr("height", barHeight * data.length); // compute height based on size of dataset
+
+// g element contains a rect and a text
+// g element for each data point
+var bar = chart.selectAll("g")
+    .data(data)
+  .enter().append("g")
+    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+// graph appears
+bar.append("rect")
+    .attr("width", x)
+    .attr("height", barHeight - 1);
+
+// text alignment
+bar.append("text")
+    .attr("x", function(d) { return x(d) - 3; }) // moves to the edge of the bar
+    .attr("y", barHeight / 2) // moves to right corner of the bar
+    .attr("dy", ".35em") // moves to center of bar
+    .text(function(d) { return d; });
