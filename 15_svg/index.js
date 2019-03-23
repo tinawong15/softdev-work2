8a201118e18e1239030743dbc004d57405cafa57
tinawong15 <tinaw425@gmail.com>
2019-03-23 18:46:40 -0400

    mods to 15

--- 15_svg/index.js
+++ 15_svg/index.js
@@ -1,89 +1,66 @@
-var data = [];
+//Source for data: http://www.statpoint.net/default.aspx
+
 var tasks = [1, 2, 3, 4, 5, 6]
-var values = [39, 40, 44, 47, 41, 37]
+var values =[39, 40, 44, 47, 41, 37]
+
+var width = 500;
+var height = 400;
+var svg = d3.select(".chart").attr("width", width).attr("height", height)
 
 var i = 0;
-for(i = 0; i < tasks.length; i++) {
-  data[i] = [tasks[i], values[i]];
-  console.log(data[i])
+while (i < tasks.length){
+  var circle = svg.append("circle")
+	.attr("cx", function(d){return 50+ 50*tasks[i]})
+	.attr("cy", function(d){return 5 * values[i]})
+	.attr("r", 5);
+  i += 1;
 }
 
-var width = 420,
-    height = 100;
-
-var x = d3.scaleLinear()
-    .domain([1, 6])
-    .range([20, width]);
-
-var y = d3.scaleLinear()
-    .domain([30, 50])
-    .range([5, height]);
-
-var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", height);
-
-var scatter = chart.selectAll("g")
-    .data(data)
-    .enter().append("g")
-    .attr("transform", function(d) {
-      return "translate(" + x(d[0]) + "," + y(d[1]) + ")";
-     })
-     .attr('text', function(d) { return d[1] });
-
-scatter.append('rect')
-  .attr('width', 5)
-  .attr('height', 5 );
 
 var x_scale = d3.scaleLinear()
-  .domain([d3.min(tasks), d3.max(tasks)])
-  .range([0, width]);
+    .domain([d3.min(tasks),d3.max(tasks)])
+    .range([0, width]);
 
 var y_scale = d3.scaleLinear()
-  .domain([d3.min(values), d3.max(values)])
-  .range([0, height]);
+    .domain([d3.min(values),d3.max(values)])
+    .range([0, height]);
 
 var x_axis = d3.axisBottom()
-  .scale(x_scale);
+    .scale(x_scale)
 
 var y_axis = d3.axisLeft()
-  .scale(y_scale);
+    .scale(y_scale)
+
+var chart = d3.select(".chart")
+chart.append('g')
+    .attr("transform","translate (50,370)")
+    .call(x_axis)
 
 chart.append('g')
-  .attr('transform', 'translate(10,90)')
-  .call(x_axis);
+    .attr("transform","translate (50,50)")
+    .call(y_axis)
 
-chart.append('g')
-  .attr('transform', 'translate(20,0)')
-  .call(y_axis);
+chart.append("text")
+    .attr("x",width / 2)
+    .attr("y", 0)
+    .attr("dy","2em")
+    .text("Pulse Rates over Different Tasks");
 
-// var width = 420,
-//     barHeight = 20;
-//
-// var x = d3.scaleLinear()
-//     .domain([0, d3.max(data)])
-//     .range([0, width]);
-//
-// var chart = d3.select(".chart")
-//     .attr("width", width)
-//     .attr("height", barHeight * data.length); // compute height based on size of dataset
-//
-// // g element contains a rect and a text
-// // g element for each data point
-// var bar = chart.selectAll("g")
-//     .data(data)
-//   .enter().append("g")
-//     .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-//
-// // graph appears
-// bar.append("rect")
-//     .attr("width", x)
-//     .attr("height", barHeight - 1);
-//
-// // text alignment
-// bar.append("text")
-//     .attr("x", function(d) { return x(d) - 3; }) // moves to the edge of the bar
-//     .attr("y", barHeight / 2) // moves to right corner of the bar
-//     .attr("dy", ".35em") // moves to center of bar
-//     .text(function(d) { return d; });
-//
+chart.append("text")
+    .attr("x",width)
+    .attr("y", 0)
+    .attr("dy","2em")
+    .text("By Emily Lee, Tina Wong");
+
+chart.append("text")
+    .attr("x",width/2)
+    .attr("y",height-25)
+    .attr("dy","2em")
+    .text("Task");
+
+chart.append("text")
+    .attr("transform","rotate(-90)")
+    .attr("x",-200)
+    .attr("y",10)
+    .attr("dy","1em")
+    .text("Beats per Minute");
